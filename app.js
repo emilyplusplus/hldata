4186d88ff1e95088ecb85afe12652c46dc542291
Emily Carlsen <emilytcyt@gmail.com>
2017-12-12 15:11:35 -0500

    Alpha localhost

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+let db = mongoose.connect('mongodb://localhost/climate')
+
+let schema = mongoose.Schema({
+  temp: Number,
+  lux: Number,
+  time: Number
+})
+let Record = mongoose.model('Record', schema)
 
 const app = express()
-app.get('/', (req, res) => res.json({working: true}))
+app.use(bodyParser.json())
+app.post('/record', (req, res) => {
+  res.end()
+
+  req.body.time = Date.now()
+  new Record(req.body).save(function (err, r) {
+    if (err) return console.error(err)
+  })
+})
+
+app.get('/records', (req, res) => {
+  Record.find({}, (err, data) => {
+    //console.log(data)
+    //res.end()
+    res.json(data)
+  })
+})
 
 app.listen(8080, '0.0.0.0')
